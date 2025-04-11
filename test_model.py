3b3eefa8012bba01b8417c31fd4bd8556d81f390
Sebastian Dancau <sebidancau@yahoo.com>
2025-04-12 01:04:22 +0300

    mlops pipeline

---
+++ test_model.py
@@ -0,0 +1,12 @@
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+iris = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2)
+
+model = LogisticRegression(max_iter=200)
+model.fit(X_train, y_train)
+accuracy = model.score(X_test, y_test)
+
+assert accuracy > 0.5, f"Model accuracy too low: {accuracy}"
